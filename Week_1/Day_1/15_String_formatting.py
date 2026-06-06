5051af88a3bf5bc26a894f16699f0bbec9757b46
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-07 00:06:08 +0500

    Week 1 - Day 1: added 15_string_formatting

---
+++ Week_1/Day_1/15_String_formatting.py
@@ -0,0 +1,15 @@
+# String formatting
+name = "Waqar"
+age = 25
+print("My name is {} and I am {} years old.".format(name, age))
+
+# Using f-strings (Python 3.6+)
+print(f"My name is {name} and I am {age} years old.")
+
+# Using % formatting
+print("My name is %s and I am %d years old." % (name, age))
+
+# Output:
+# My name is Waqar and I am 25 years old.
+# My name is Waqar and I am 25 years old.
+# My name is Waqar and I am 25 years old.
