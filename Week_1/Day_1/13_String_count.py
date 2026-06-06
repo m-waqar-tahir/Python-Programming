72eee46fd5aedb8f3ad9be7df0cec1e11b02a8f6
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-06 23:48:40 +0500

    Week 1 - Day 1: added 13_string_count

---
+++ Week_1/Day_1/13_String_count.py
@@ -0,0 +1,5 @@
+# Count the occurrences of a substring in a string using the count() method
+text = "Hello, I have a cute cat named Dorothy. I love my cat!"
+print(text.count("o"))  # Output: 4
+print(text.count("cat"))  # Output: 2
+print(text.count("Dorothy"))  # Output: 1
