d611a6b78dd0e6f1c21465a4d02ef4ccc3692641
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-06 12:28:49 +0500

    Week 1 - Day 1: added 02_string_length

---
+++ Week_1/Day_1/02_String_length.py
@@ -0,0 +1,7 @@
+# Find length of a string using len() function
+my_string = "Hello, Waqar Tahir!"
+length_of_string = len(my_string)
+print("The length of the string is:", length_of_string)
+
+# Output:
+#  The length of the string is: 19
