f60082d37356a85e02dc1cd646260bb66d028e12
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-06 12:35:45 +0500

    Week 1 - Day 1: added 03_string_indexing

---
+++ Week_1/Day_1/03_String_indexing.py
@@ -0,0 +1,14 @@
+# Access characters by index
+my_string = "Hello, Waqar Tahir!"
+print("The first character is:", my_string[0])
+print("The fifth character is:", my_string[4])
+
+# Negative indexing
+print("The last character is:", my_string[-1])
+print("The second-to-last character is:", my_string[-2])
+
+# Output:
+# The first character is: H
+# The fifth character is: o
+# The last character is: !
+# The second-to-last character is: r
