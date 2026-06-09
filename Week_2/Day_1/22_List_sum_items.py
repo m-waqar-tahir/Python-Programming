49118790ac15d084ce601d38ca8fac587d7abd41
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-09 17:59:35 +0500

    Week 2 - Day 1: added 22_List_sum_items

---
+++ Week_2/Day_1/22_List_sum_items.py
@@ -0,0 +1,14 @@
+# Sum of List items
+list1 = [10, 20, 30, 40, 50]
+total = sum(list1)
+print("The sum of the list items is:", total)
+
+# Sum of items in multiple lists
+list2 = [5, 15, 25]
+total_sum = list1 + list2
+total_sum = sum(total_sum)
+print("The sum of the items in both lists is:", total_sum)
+
+# Output:
+# The sum of the list items is: 150
+# The sum of the items in both lists is: 180
