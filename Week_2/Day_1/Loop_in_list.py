51922c5c670b3f0391b5cbf6ea41b86ae16da333
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-10 18:30:31 +0500

    40

---
+++ Week_2/Day_1/Loop_in_list.py
@@ -0,0 +1,15 @@
+# Loops in a list
+
+# for is a loop keyword, list1 has n number of items in it. These n items stored in x and printed one by one.
+list1 = ["a", "b", "c", [1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+for x in list1:
+    print(x)
+
+# Output:
+# a
+# b
+# c
+# [1, 2, 3]
+# [4, 5, 6]
+# [7, 8, 9]
