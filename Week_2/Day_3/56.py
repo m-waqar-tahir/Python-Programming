b87a3e470e80058145c8de48236e33b5b67776f4
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-12 16:13:18 +0500

    updated

--- Week_2/Day_3/56.py
+++ Week_2/Day_3/56.py
@@ -1,11 +1,12 @@
-# break Statement with for Loop
+# Continue Statement with for Loop
 
 for i in range(5):
     if i == 3:
-        break
+        continue
     print(i)
 
 # Output:
-# 0
 # 1
 # 2
+# 3
+# 4
