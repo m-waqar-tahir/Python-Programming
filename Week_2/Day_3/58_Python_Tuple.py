b76fd2ff3f6e5e58308a66ae71a884e4944d3597
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-12 17:41:38 +0500

    .

---
+++ Week_2/Day_3/58_Python_Tuple.py
@@ -0,0 +1,11 @@
+# Tuples in Python
+
+t = (1, 2, 3)
+t1 = 1, 2, 3
+
+print(type(t))
+print(type(t1))
+
+# Output:
+# <class 'tuple'> means the type is tuple.
+# <class 'tuple'> This is also a tuple.
