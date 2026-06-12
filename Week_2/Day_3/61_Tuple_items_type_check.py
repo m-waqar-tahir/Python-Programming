0d42c0eae16d53129bf0387c0df7d57b93e0dcc1
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-12 17:51:41 +0500

    .

---
+++ Week_2/Day_3/61_Tuple_items_type_check.py
@@ -0,0 +1,13 @@
+# Tuples items type check
+
+t = (1, 2.5, 3)
+t1 = "I", 'am', 25, 'years', 'old.'
+
+print(type(t[1]))
+print(type(t1[0]))
+print(type(t1[2]))
+
+# Output:
+# <class 'float'> 
+# <class 'str'> 
+# <class 'int'>
