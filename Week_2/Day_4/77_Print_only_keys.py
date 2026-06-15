e684e13ad2928f8ea3e42bd5496357f751ee43bf
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-15 13:01:57 +0500

    .

---
+++ Week_2/Day_4/77_Print_only_keys.py
@@ -0,0 +1,8 @@
+# Printing only keys present in a dictionary without printing values.
+
+student = {"name": "Waqar", "age": 25, "city": "Faisalabad"}
+
+print("The keys of the dictionary are: ", student.keys())
+
+# Output:
+# The keys of the dictionary are: dict_keys(['name', 'age', 'city'])
