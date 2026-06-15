2a347fa98bcddc98e8eb701e9444ac14877c1465
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-15 12:51:11 +0500

    .

---
+++ Week_2/Day_4/74_Adding_new_pair.py
@@ -0,0 +1,10 @@
+# Adding new Key: Value pair in a dictionary
+
+student = {"name": "Waqar", "age": 25}
+
+student["city"] = "Faisalabad"
+
+print(student)
+
+# Output: 
+# {'name': 'Waqar', 'age': 25, 'city': 'Faisalabad'}
