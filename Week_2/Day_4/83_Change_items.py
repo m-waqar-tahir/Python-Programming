d89d303ef42341f2f19d25321391a19ea0cffb6a
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-15 18:33:36 +0500

    .

---
+++ Week_2/Day_4/83_Change_items.py
@@ -0,0 +1,9 @@
+# Method 1: Updating items of a dictionary using key
+
+profile = {"name": "Waqar", "surname": "Tahir", "birthday_year": 2000}
+profile["birthday_year"] = 2001 # value changed through it's key.
+
+print(profile)
+
+# Output:
+# {'name': 'Waqar', 'surname': 'Tahir', 'birthday_year': 2001}
