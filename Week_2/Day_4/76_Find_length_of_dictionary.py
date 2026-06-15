313040842ff0bedb90e475370c1bbc4ac0c1bf37
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-15 12:57:35 +0500

    .

---
+++ Week_2/Day_4/76_Find_length_of_dictionary.py
@@ -0,0 +1,8 @@
+# Finding the length of a dictionary or the number of items existing in a dictionary
+
+student = {"name": "Waqar","age": 20,"city": "Faisalabad"}
+
+print("The length of the dictionary is: ", len(student))
+
+# Output:
+# The length of the dictionary is: 3
