614c7348fae754c251d062bb087953bbb16aac48
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-15 18:25:48 +0500

    .

---
+++ Week_2/Day_4/82_Loop_through_items.py
@@ -0,0 +1,10 @@
+# Loop Through Keys and Values or items of a dictionary.
+
+student = {"name": "Waqar", "age": 25}
+
+for key, value in student.items():
+    print(key, value)
+
+# Output:
+# name Waqar 
+# age 25
