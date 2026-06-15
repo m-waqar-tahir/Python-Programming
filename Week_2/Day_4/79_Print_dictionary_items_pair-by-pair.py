d74199be34ec181ca453ce20994785a3a3ce41a5
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-15 13:08:49 +0500

    .

---
+++ Week_2/Day_4/79_Print_dictionary_items_pair-by-pair.py
@@ -0,0 +1,8 @@
+# Printing keys and values as pairs by pair in the form of tuples.
+
+student = {"name": "Waqar", "age": 25}
+
+print(student.items())
+
+# Output:
+# dict_items([('name': 'Waqar') ('age': 25)])
