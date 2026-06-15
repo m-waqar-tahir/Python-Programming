622b5c3f95edd653008a4e4a5bf9d79f6a6c272c
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-15 13:04:52 +0500

    .

---
+++ Week_2/Day_4/78_Print_only_values.py
@@ -0,0 +1,8 @@
+# Printing only values present in a dictionary without printing keys.
+
+student = {"name": "Waqar", "age": 25, "city": "Faisalabad"}
+
+print(student.values())
+
+# Output:
+# dict_values(['Waqar', 25, 'Faisalabad'])
