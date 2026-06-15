49b502aeec8cce59b91a2abf8b59b7ddea7e54eb
Muhammad Waqar Tahir <waqartahir480@gmail.com>
2026-06-15 12:30:40 +0500

    .

---
+++ Week_2/Day_4/71_Dictionary_creation.py
@@ -0,0 +1,12 @@
+# Creating a Dictionary in Python
+
+student = {
+    "name": "Waqar", # Key 'name': Value 'Waqar' pair
+    "age": 25,
+    "department": "CS"
+}
+
+print(student)
+
+# Output:
+# {'name': 'Waqar', 'age': 25, 'department': 'CS'}
